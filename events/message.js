f7fc0b8de32ccb57c065ef9b5fc37a888dcd9f90
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-01-18 13:51:39 +0000

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,5 +1,7 @@
+const { MessageEmbed } = require('discord.js');
+
 exports.run = async (client, message) => {
-    if (message.author.bot || !message.guild || !message.content.startsWith(client.config.prefix)) return;
+    if (message.author.bot || !message.content.startsWith(client.config.prefix)) return;
 
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
@@ -7,12 +9,17 @@ exports.run = async (client, message) => {
     
     let command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
     if (!command) return;
+    if (!message.guild && command.guildOnly) return message.channel.send(client.errEmb('This command cant be used in DMs.'));
     
     if(command.permissions) {
         if(message.member.permissions.has(command.permissions)) {
             command.run(client, message, args);
         } else {
-            message.reply('Missing permissions.')
+            const embed = new MessageEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+            .setDescription(`You are missing \`${command.permissions.map(p => p.replace(/_/g, ' ')).join(', ')}\` permission(s).`)
+            .setColor(message.member.displayHexColor)
+            message.channel.send(embed)
         }
     } else {
         command.run(client, message, args);
