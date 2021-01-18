db92182e78007123dd5b095bd4a164bf975e20e4
tryharddeveloper <73663825+tryharddeveloper@users.noreply.github.com>
2021-01-19 01:50:31 +0530

    Mongo.js Updated. Add the login token with config

--- mongo.js
+++ mongo.js
@@ -1,1 +1,30 @@
-// WIP for Tryhard lol
+const mongoose = require('mongoose');
+
+module.exports = {
+    init: () => {
+        const dbOptions = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            autoIndex: false,
+            poolSize: 5,
+            connectTimeoutMS: 10000,
+            family:4
+        };
+
+        mongoose.connect('mongodb+srv://Admins:Admins@radeon.xecdu.mongodb.net/Radeon?retryWrites=true&w=majority', dbOptions);
+        mongoose.set('useFindAndModify', false);
+        mongoose.Promise = global.Promise;
+
+        mongoose.connection.on('connected', () => {
+            console.log('Mongoose has successfully connected!');
+        });
+
+        mongoose.connection.on('err', err => {
+            console.error(`Mongoose connection error: \n${err.stack}`);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.warn('Mongoose connection lost');
+        });
+    }
+}
