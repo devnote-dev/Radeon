5291db881351eb083fcf83c1442c1e68f668a486
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-01-19 23:01:21 +0000

    Final Fix

--- radeon.js
+++ radeon.js
@@ -5,7 +5,7 @@ const {readdirSync} = require('fs');
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.config = require('./config.json');
-client.mongoose = require('./mongo.js');
+client.mongoose = require('./mongo');
 
 readdirSync('./handlers/').forEach(handler => {
     if (!handler.endsWith('.handler.js')) return;
