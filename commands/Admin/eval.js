a7f31a31e0bcea8a64ebe878050b64f5e512d621
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-01-22 16:18:11 +0000

    Update eval.js

--- commands/Admin/eval.js
+++ commands/Admin/eval.js
@@ -1,74 +1,25 @@
-const Discord = require('discord.js')
+require('discord.js');
+const {inspect} = require('util');
 
 module.exports = {
-    name: "eval",
-    description: "Access denied!",
-    category: 'Dev',
+    name: 'eval',
+    guildOnly: true,
+    modOnly: true,
     run: async (client, message, args) => {
-        const error = new Discord.MessageEmbed()
-        .setTitle("Error!")
-        .setColor("0xc846ff")
-        .setFooter(`Used by: ${message.author.tag}`, `${message.author.displayAvatarURL({dynamic: true})}`)
-        .setTimestamp()
-    if(!["332258095264825352", "622146791659405313", "705380719882403902"].includes(message.author.id)) {
-        error.setDescription("You are not a bot owner!")
-        return message.channel.send(error);
-    }
-    const embed = new Discord.MessageEmbed()
-    .addField("Input", "```js\n" + args.join(" ") + "```");
-    if(args[0] === "stats") {
-      embed.addField("Output", `\`\`\`diff\n+ Users: ${client.users.cache.size}\n+ Channels: ${client.channels.cache.size}\n+ Guilds: ${client.guilds.cache.size}\n\`\`\``)
-      embed.setColor(0xc846ff)
-      return message.channel.send(embed);
-    }
-    try {
-        const code = args.join(" ");
-            if (!code) { 
-                error.setDescription("Please include the code.")
-                return message.channel.send(error)
+        if (!client.config.botOwners.includes(message.author.id)) return;
+        if (args.length < 1) return message.channel.send('<:wtf_dude:789567331495968818>');
+        const code = args.join(' ');
+        try {
+            const evaled = eval(code);
+            const i = (s,d=0) => inspect(s,false,d);
+            const m = await message.channel.send('```js\n'+ i(evaled).slice(0,1980) +'\n```');
+            if(evaled instanceof Promise){
+                await evaled;
+                return m.edit('```js\n'+ i(evaled,1).slice(0,1980) +'\n```');
             }
-                let evaled;
-    
-    // This method is to prevent someone that you trust, open the secret shit here.
-    if (code.includes(`SECRET`) || code.includes(`TOKEN`) || code.includes("process.env")) {
-      evaled = "No, shut up, what will you do it with the token?";
-    } else {
-      evaled = eval(code);
-    }
-    
-    if (typeof evaled !== "string") evaled = require("util").inspect(evaled, {depth: 0});
-    
-    let output = clean(evaled);
-    if (output.length > 1024) {
-      // If the output was more than 1024 characters, we're gonna export them into the hastebin.
-      const {body} = await post("https://hastebin.com/documents").send(output);
-      embed.addField("Output", `https://hastebin.com/${body.key}.js`).setColor(0xc846ff);
-      // Sometimes, the body.key will turn into undefined. It might be the API is under maintenance or broken.
-    } else {
-      embed.addField("Output", "```js\n" + output + "```").setColor(0xc846ff)
-    }
-    
-    message.channel.send(embed);
-    
-  } catch (error) {
-            let err = clean(error);
-            if (err.length > 1024) {
-                const {body} = await post("https://hastebin.com/documents").send(err);
-                embed.addField("Output", `https://hastebin.com/${body.key}.js`).setColor("RED");
-            } else {
-                embed.addField("Output", "```js\n" + err + "```").setColor("RED");
-            }
-    
-            message.channel.send(embed);
+        } catch (error) {
+            message.channel.send(`\`\`\`js\n${error}\n\`\`\``);
         }
+        console.log(`${message.guild.id}/${message.author.id}: Eval:\n${code}`);
     }
 }
-
-function clean(string) {
-    if (typeof text === "string") {
-      return string.replace(/`/g, "`" + String.fromCharCode(8203))
-      .replace(/@/g, "@" + String.fromCharCode(8203))
-    } else {
-      return string;
-    }
-  }
