82d4886e79cd1ed85ea6c9a18607af0fecd76ecd
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-01-24 17:19:52 +0000

    Create guildDelete.js

---
+++ events/guildDelete.js
@@ -0,0 +1,12 @@
+const {MessageEmbed} = require('discord.js');
+const Guild = require('../schemas/guild-schema');
+
+exports.run = async (client, guild) => {
+    const e = new MessageEmbed()
+    .setDescription(`<:crossred:796925441490681889> Left **${guild.name}** - Active in ${client.guilds.cache.size} Servers!`)
+    .setColor(0xd10000)
+    .setTimestamp();
+    client.channels.cache.get(client.config.logs.guilds).send(e);
+    Guild.findOneAndRemove({guildID: guild.id}).catch(()=>{});
+    console.log(`Mongoose | Guild Removed: ${guild.name}`);
+}
