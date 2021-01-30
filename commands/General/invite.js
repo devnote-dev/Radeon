20a1dcaf9ba94fbf266c9842c2713597c55b325f
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-01-30 17:38:34 +0000

    Create invite.js

---
+++ commands/General/invite.js
@@ -0,0 +1,17 @@
+const {MessageEmbed} = require('discord.js');
+
+module.exports = {
+    name: 'invite',
+    aliases: ['support'],
+    description: 'Sends Radeon\'s invite links!',
+    guildOnly: false,
+    run: async (client, message) => {
+        const e = new MessageEmbed()
+        .setTitle('🔗 Invite Links')
+        .setThumbnail(client.user.displayAvatarURL())
+        .setDescription('• [Bot Invite](https://discord.com/api/oauth2/authorize?client_id=762359941121048616&permissions=8&scope=bot)\n• [Support Server](https://discord.gg/xcZwGhSy4G)')
+        .setColor(0x1e143b)
+        .setFooter(`Triggered By ${message.author.tag}`, message.author.avatarURL());
+        message.channel.send(e);
+    }
+}
