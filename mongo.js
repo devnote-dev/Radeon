cc899cebc8926958175abc80309096ea7806b613
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-01-30 17:50:08 +0000

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,30 +1,25 @@
 const mongoose = require('mongoose');
 
 module.exports = {
-    init: () => {
+    init: async () => {
         const dbOptions = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            autoIndex: false,
-            poolSize: 5,
-            connectTimeoutMS: 10000,
-            family:4
+            useFindAndModify: false
         };
 
-        mongoose.connect('mongodb+srv://Admins:Admins@radeon.xecdu.mongodb.net/Radeon?retryWrites=true&w=majority', dbOptions);
-        mongoose.set('useFindAndModify', false);
-        mongoose.Promise = global.Promise;
-
         mongoose.connection.on('connected', () => {
-            console.log('Mongoose has successfully connected!');
+            console.log('Mongoose | \x1b[32mSuccessfully Connected\x1b[0m');
         });
 
         mongoose.connection.on('err', err => {
-            console.error(`Mongoose connection error: \n${err.stack}`);
+            console.error(`Mongoose | \x1b[33mConnection Error:\x1b[0m\n${err.stack}`);
         });
 
         mongoose.connection.on('disconnected', () => {
-            console.warn('Mongoose connection lost');
+            console.warn('Mongoose | \x1b[31mConnection Lost\x1b[0m');
         });
+
+        await mongoose.connect('mongodb+srv://Admins:Admins@radeon.xecdu.mongodb.net/Radeon?retryWrites=true&w=majority', dbOptions);
     }
 }
