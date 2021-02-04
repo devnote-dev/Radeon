2b5ca716399c2a7d24672af444a75bb410547664
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-02-04 16:48:05 +0000

    Update shardReconnecting.js

--- events/shardReconnecting.js
+++ events/shardReconnecting.js
@@ -3,7 +3,7 @@ const {MessageEmbed} = require('discord.js');
 exports.run = async (client, shard) => {
     const e = new MessageEmbed()
     .setTitle('Radeon')
-    .setDescription(`${shard} / 1 - Reconnecting`)
+    .setDescription(`Shard ${shard} / ${client.shard.count} - Reconnecting`)
     .setColor('YELLOW');
     client.channels.cache.get(client.config.logs.event).send(e);
 }
