3fa5ed1d877f7399f9e4ef5921c27b42650ecce4
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-02-04 16:47:42 +0000

    Update shardReady.js

--- events/shardReady.js
+++ events/shardReady.js
@@ -3,7 +3,7 @@ const {MessageEmbed} = require('discord.js');
 exports.run = async (client, shard) => {
     const e = new MessageEmbed()
     .setTitle('Radeon')
-    .setDescription(`${shard} / 1 - Ready`)
+    .setDescription(`Shard ${shard} / ${client.shard.count} - Ready`)
     .setColor('GREEN');
     client.channels.cache.get(client.config.logs.event).send(e);
 }
