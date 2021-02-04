4578eb648a22777b206524da26d747e0363e5df8
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-02-04 16:46:49 +0000

    Update shardDisconnect.js

--- events/shardDisconnect.js
+++ events/shardDisconnect.js
@@ -1,9 +1,9 @@
 const {MessageEmbed} = require('discord.js');
 
-exports.run = async (client, shard) => {
+exports.run = async (client, event, shard) => {
     const e = new MessageEmbed()
     .setTitle('Radeon')
-    .setDescription(`${shard} / 1 - Disconnected`)
+    .setDescription(`Shard ${shard} / ${client.shard.count} - Disconnected`)
     .setColor('RED');
     client.channels.cache.get(client.config.logs.event).send(e);
 }
