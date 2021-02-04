269ee26b134bf419402e02ca165ff960e03e63ad
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-02-04 16:47:15 +0000

    Create shardError.js

---
+++ events/shardError.js
@@ -0,0 +1,9 @@
+const {MessageEmbed} = require('discord.js');
+
+exports.run = async (client, error, shard) => {
+    const e = new MessageEmbed()
+    .setTitle('Radeon')
+    .setDescription(`Shard ${shard} / ${client.shard.count} - Error\n${error.message}`)
+    .setColor('DARK_RED');
+    client.channels.cache.get(client.config.logs.event).send(e);
+}
