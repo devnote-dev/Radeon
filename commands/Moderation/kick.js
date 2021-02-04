4cf96262c4228af0cb529e0261698644a70606a8
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-02-04 16:37:11 +0000

    Update kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -7,18 +7,18 @@ module.exports = {
     guildOnly: true,
     permissions: ['KICK_MEMBERS'],
     run: async (client, message, args) => {
-        if (args.length < 1) return message.channel.send(client.errEmb('No User Specified.\n```\nkick <User:Mention/ID> [Reason:text]\n```'));
+        if (args.length < 1) return client.errEmb('No User Specified.\n```\nkick <User:Mention/ID> [Reason:text]\n```', message);
         const target = message.mentions.members.first() || message.guild.member(args[0]);
-        if (!target) return message.channel.send(client.errEmb('User is not a member of this server.'));
+        if (!target) return client.errEmb('User is not a member of this server.', message);
         let reason = '(No Reason Specified)';
         if (args.length > 1) reason = args.slice(1).join(' ');
-        if (!target.kickable) return message.channel.send(client.errEmb(`User \`${target.user.tag}\` could not be kicked (user may have a role higher than the bot).`));
+        if (!target.kickable) return client.errEmb(`User \`${target.user.tag}\` could not be kicked (user may have a role higher than the bot).`, message);
         try {
             await target.send({embed:{title:'You have been kicked!',description:`**Reason:** ${reason}`,color:0x1e143b,footer:{text:`Sent from ${message.guild.name}`, icon_url:message.guild.iconURL({dynamic: true})}}}).catch(()=>{});
             target.kick(message.author.tag +': '+ reason);
-            return message.channel.send(client.successEmb(`Successfully Kicked \`${target.user.tag}\`!`));
+            return client.checkEmb(`Successfully Kicked \`${target.user.tag}\`!`, message);
         } catch (err) {
-            return message.channel.send(client.errEmb(`Unknown: Failed Kicking Member \`${target.user.tag}\``));
+            return client.errEmb(`Unknown: Failed Kicking Member \`${target.user.tag}\``, message);
         }
     }
 }
