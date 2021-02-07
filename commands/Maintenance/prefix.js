ba7686a4c7f0811979148ac73d4681f9b30920ea
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-02-07 20:53:29 +0000

    Create prefix.js

---
+++ commands/Maintenance/prefix.js
@@ -0,0 +1,21 @@
+const {MessageEmbed} = require('discord.js');
+const Guild = require('../../schemas/guild-schema');
+
+module.exports = {
+    name: 'prefix',
+    desription: 'Shows the current prefix for the server.',
+    guildOnly: true,
+    run: async (client, message) => {
+        try {
+            const data = await Guild.findOne({guildID: message.guild.id});
+            const embed = new MessageEmbed()
+            .setDescription(`Current Prefix: \`${data.prefix}\`\n\nDefault Prefixes: \`r!\`, <@${client.user.id}>`)
+            .setColor(0x1e143b)
+            .setFooter('Note: DM commands do not require a prefix.');
+            message.channel.send(embed);
+        } catch (err) {
+            console.error(err);
+            client.errEmb('This command cant be used at this time.', message);
+        }
+    }
+}
