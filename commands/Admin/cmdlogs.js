edbbc85224c433950f899b72ccc0f07999160f65
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-02-07 20:51:28 +0000

    Create cmdlogs.js

---
+++ commands/Admin/cmdlogs.js
@@ -0,0 +1,26 @@
+const {MessageAttachment} = require('discord.js');
+const {readFileSync, writeFileSync} = require('fs');
+const {join} = require('path');
+
+module.exports = {
+    name: 'cmdlogs',
+    guildOnly: true,
+    modOnly: true,
+    run: async (client, message) => {
+        let content = '';
+        client.cmdlogs.forEach(log => {
+            content += `USER: ${log.user}\nCMD: ${log.command}\nCHANNEL: ${log.channel.id} - ${log.channel.type}\nTIME: ${log.time}\n\n`;
+        });
+        writeFileSync(join(__dirname,'/','_logs.txt'),content,{flag:'w+'},err => console.error(err));
+        const buff = readFileSync(join(__dirname,'/','_logs.txt'));
+        const att = new MessageAttachment(buff,'Radeon_cmdlogs.txt');
+        try {
+            message.author.send('Radeon Command Logs',att);
+            const msg = await client.infoEmb(`${message.author} I have DMed you the file.\nThis message will autodelete in 5 seconds.`, message);
+            msg.delete({timeout:5000});
+        } catch (err) {
+            client.errEmb(err.message);
+        }
+        message.delete();
+    }
+}
