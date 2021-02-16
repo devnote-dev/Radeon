6532a9602a463b9cecbce547dd34e0d1e0daa62f
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-02-16 17:17:17 +0000

    Update shardCreate.js

--- events/shardCreate.js
+++ events/shardCreate.js
@@ -5,5 +5,5 @@ exports.run = async (client, shard) => {
     .setTitle('Radeon')
     .setDescription(`Shard ${shard} / ${client.shard.count} - Ready`)
     .setColor('PURPLE');
-    client.channels.cache.get(client.config.logs.event).send(e);
+    client.channels.cache.get(client.config.logs.event).send(e).catch(()=>{});
 }
