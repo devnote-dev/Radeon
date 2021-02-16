6ba954fd6e5f61d0d9678b756f539f657abc9aca
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-02-16 17:04:25 +0000

    Create dbupdate.js

---
+++ commands/Admin/dbupdate.js
@@ -0,0 +1,21 @@
+require('discord.js');
+
+module.exports = {
+    name: 'dbupdate',
+    description: 'Updates the database of a specified guild to the latest schema update.',
+    usage: 'dbupdate <Guild:ID>',
+    guildOnly: true,
+    modOnly: 'warn',
+    run: async (client, message, args) => {
+        if (!args.length) return client.errEmb('No Guild Specified.\n```\ndbupdate <Guild:ID>\n```', message);
+        const server = client.guilds.cache.get(args[0]);
+        if (!server) return client.errEmb('Unknown Guild Specified.', message);
+        try {
+            await client.emit('guildDelete', server);
+            await client.emit('guildCreate', server);
+            return client.checkEmb(`Successfully Updated Database for Guild: \`${server.name}\`!`, message);
+        } catch (err) {
+            return client.errEmb(err.message, message);
+        }
+    }
+}
