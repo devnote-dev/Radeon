8abb3d2d5bf2c5e8a6ba7b889fa52572f186a489
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-03-01 05:17:07 +0000

    Create editembed.js

---
+++ commands/General/editembed.js
@@ -0,0 +1,23 @@
+require('discord.js');
+
+module.exports = {
+    name: 'editembed',
+    aliases: ['editemb'],
+    description: 'Edits an existing embed sent by Radeon using JSON string.',
+    usage: 'editembed <Message:ID> <JSON:Text>',
+    guildOnly: true,
+    permissions: 8192,
+    run: async (client, message, args) => {
+        if (args.length < 2) return client.errEmb('Insufficient Arguments Specified.\n```\neditembed <Message:ID> <JSON:Text>\n```', message);
+        const msg = message.channel.messages.cache.get(args[0]);
+        if (!msg) return client.errEmb('Invalid Message.\nEither the message ID provided was invalid or the message is not in the bot\'s cache.', message);
+        if (msg.author.id != client.user.id) return client.errEmb('Message was not sent by this bot.', message);
+        try {
+            const embed = JSON.parse(args.slice(1).join());
+            await msg.edit({embed:embed});
+            await message.react(':checkgreen:796925441771438080').catch(()=>{});
+        } catch (err) {
+            client.errEmb(err.message, message);
+        }
+    }
+}
