43625a27b46d188f5120d72c897d44d3d16898fd
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-03-01 05:13:53 +0000

    Update shutdown.js

--- commands/Admin/shutdown.js
+++ commands/Admin/shutdown.js
@@ -2,11 +2,12 @@ require('discord.js');
 
 module.exports = {
     name: 'shutdown',
+    aliases: ['restart'],
     description: 'Shuts down existing instances of Radeon. Note: this may result in the Client restarting if the host is operating off Batch File.',
     guildOnly: true,
     modOnly: 'void',
     run: async (client, message) => {
-        await message.react('a:loading:786661451385274368');
+        await message.react('a:loading:786661451385274368').catch(()=>{});
         process.exit(0);
     }
 }
