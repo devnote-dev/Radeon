a14a55c471da05facdc588185cfc769047c331ba
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-03-01 15:24:49 +0000

    Update and rename radeon.js to Radeon.js

--- Radeon.js
+++ Radeon.js
@@ -1,11 +1,14 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({
+    partials:['CHANNEL','GUILD_MEMBER','MESSAGE','USER'],
+    allowedMentions:{ parse:['users'] }
+});
 const {readdirSync} = require('fs');
 
 client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-client.cmdlogs = new Set();
-client.config = require('./config.json');
+client.aliases  = new Discord.Collection();
+client.cmdlogs  = new Set();
+client.config   = require('./config.json');
 client.mongoose = require('./mongo');
 
 readdirSync('./handlers/').forEach(handler => {
