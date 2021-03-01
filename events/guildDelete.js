9eeeaede94dbde244b946202bb5f6f6a1ea91ed4
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-03-01 15:17:58 +0000

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -8,7 +8,7 @@ exports.run = async (client, guild) => {
     .setColor(0xd10000)
     .setTimestamp();
     client.channels.cache.get(client.config.logs.joins).send(e).catch(()=>{});
-    Guild.findOneAndDelete({guildID: guild.id}).catch(()=>{});
+    await Guild.findOneAndDelete({guildID: guild.id}).catch(()=>{});
     console.log(`Mongoose | Guild Removed: ${guild.name}`);
-    Muted.findOneAndDelete({guildID: guild.id}).catch(()=>{});
+    await Muted.findOneAndDelete({guildID: guild.id}).catch(()=>{});
 }
