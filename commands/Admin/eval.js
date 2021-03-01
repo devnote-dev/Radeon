146caf924b722fa883d9f3b75ec0ccede341e3e3
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-03-01 05:12:55 +0000

    Update eval.js

--- commands/Admin/eval.js
+++ commands/Admin/eval.js
@@ -7,10 +7,11 @@ module.exports = {
     modOnly: 'void',
     run: async (client, message, args) => {
         if (args.length < 1) return client.errEmb('No Code Provided.', message);
-        const code = args.join(' ');
+        let code = args.join(' ');
         if (/token|(client)?\.config/gi.test(code)) return client.errEmb('You Can\'t Do That.', message);
+        if (/^`{3}(?:.+)?\n*?[\s\S]+\n*`{3}$/gmi.test(code)) code = code.replace(/^`{3}(?:.+)?\n|\n?`{3}$/gmi, '');
         try {
-            const evaled = eval(code);
+            const evaled = await eval(code);
             const i = (s,d=0) => inspect(s,false,d);
             const m = await message.channel.send('```js\n'+ i(evaled).slice(0,1980) +'\n```');
             if(evaled instanceof Promise){
