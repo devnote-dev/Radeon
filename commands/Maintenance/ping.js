64742191f0d08014d0f80c1168ddac6951a11eba
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-03-01 05:23:27 +0000

    Update ping.js

--- commands/Maintenance/ping.js
+++ commands/Maintenance/ping.js
@@ -3,7 +3,6 @@ require('discord.js');
 module.exports = {
     name: 'ping',
     description: 'Sends Radeon\'s ping.',
-    guildOnly: false,
     run: async (client, message) => {
         const msg = await message.channel.send({embed:{color:0x1e143b,thumbnail:{url:'https://cdn.discordapp.com/emojis/786661451385274368.gif?v=1'}}});
         msg.edit({embed:{title:'🏓 Pong!',description:`Websocket: ${client.ws.ping}ms\nAPI: ${msg.createdTimestamp - message.createdTimestamp}ms`,color:0x1e143b}});
