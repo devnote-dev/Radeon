684e3d7fb8b07d45c7d016e7baf9e45662cb8042
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-03-05 13:57:48 +0000

    Update jsonembed.js

--- commands/General/jsonembed.js
+++ commands/General/jsonembed.js
@@ -5,11 +5,12 @@ module.exports = {
     aliases: ['jsonemb','jsemb'],
     description: 'Creates an embed using JSON string.',
     usage: 'jsonembed <JSON:Text>\n\njsonembed { "title":"hi", "description":"hello" }',
+    cooldown: 2,
     guildOnly: true,
     run: async (client, message, args) => {
         if (!args.length) return client.errEmb('No JSON Provided.\n```\njsonembed <JSON:Text>\n```', message);
         try {
-            const embed = JSON.parse(args.join());
+            const embed = JSON.parse(args.join(' '));
             message.channel.send({embed:embed});
         } catch (err) {
             client.errEmb(err.message, message);
