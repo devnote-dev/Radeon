03e4636c4641f854b049a6f001fb1d3856c43eec
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-03-05 14:34:57 +0000

    Update Radeon.js

--- Radeon.js
+++ Radeon.js
@@ -1,20 +1,43 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({
-    partials:['CHANNEL','GUILD_MEMBER','MESSAGE','USER'],
-    allowedMentions:{ parse:['users'] }
+    ws:{
+        intents:[
+            'DIRECT_MESSAGES',
+            'GUILDS',
+            'GUILD_BANS',
+            'GUILD_EMOJIS',
+            'GUILD_INVITES',
+            'GUILD_MEMBERS',
+            'GUILD_MESSAGES',
+            'GUILD_PRESENCES',
+        ]
+    },
+    partials:[
+        'CHANNEL',
+        'GUILD_MEMBER',
+        'MESSAGE',
+        'USER'
+    ],
+    allowedMentions:{
+        parse:['users']
+    }
 });
-const {readdirSync} = require('fs');
+const { readdirSync } = require('fs');
 
-client.commands = new Discord.Collection();
-client.aliases  = new Discord.Collection();
-client.cmdlogs  = new Set();
-client.config   = require('./config.json');
-client.mongoose = require('./mongo');
+client.commands  = new Discord.Collection();
+client.aliases   = new Discord.Collection();
+client.cmdlogs   = new Set();
+client.cooldowns = new Map();
+client.rlcount   = 0;
+client.config    = require('./config.json');
+client.mongoose  = require('./mongo');
 
 readdirSync('./handlers/').forEach(handler => {
     if (!handler.endsWith('.handler.js')) return;
     require(`./handlers/${handler}`)(client);
 });
+
+setInterval(() => { client.rlcount = 0 }, 600000);
 
 client.mongoose.init();
 client.login(client.config.token);
