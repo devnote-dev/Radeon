fb325bab86610fdea1809c80af1ab8bef8b959e4
NaruDevnote <pokeraven4@gmail.com>
2021-03-17 20:19:52 +0000

    fix(cmds): RegExp bug with eval.js

--- commands/Admin/eval.js
+++ commands/Admin/eval.js
@@ -10,7 +10,7 @@ module.exports = {
         if (!args.length) return client.errEmb('No Code Provided.', message);
         const path = `${message.guild.id}/${message.channel.id}`;
         let code = args.join(' ');
-        if (/^`{3}(?:.+)?\n*?[\s\S]+\n*`{3}$/gmi.test(code)) code = code.replace(/^`{3}(?:.+)?\n|\n?`{3}$/gmi, '');
+        if (/^`{3}(?:.+)?(?:\n+)?[\s\S]+(?:\n+)?`{3}$/gmi.test(code)) code = code.replace(/^```(?:[\S\n]+)?|```$/gmi, '');
         logAdmin('eval', path, message.author.id, code);
         if (/token|(client)?\.config|while\s*\(\s*true\s*\)\s*\{\s*\}/gi.test(code)) return client.errEmb('You Can\'t Do That.', message);
         try {
