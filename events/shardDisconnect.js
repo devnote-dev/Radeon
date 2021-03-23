07abe24e3aacaec57b3a1ba0d0056e1977a4cca4
NaruDevnote <pokeraven4@gmail.com>
2021-03-23 21:01:15 +0000

    refactor(events): Shard error handling & ConsoleR integration

--- events/shardDisconnect.js
+++ events/shardDisconnect.js
@@ -1,9 +1,13 @@
 const { MessageEmbed } = require('discord.js');
+const { logShard, logWarn } = require('../console/consoleR');
 
 exports.run = async (client, event, shard) => {
+    logShard(client, 'discon', shard);
+    logWarn(event);
     const e = new MessageEmbed()
     .setTitle('Radeon')
     .setDescription(`Shard ${shard} / ${client.shard.count} - Disconnected`)
     .setColor('RED');
-    client.channels.cache.get(client.config.logs.event).send(e).catch(()=>{});
+    const c = client.channels.cache.get(client.config.logs.event);
+    if (c) c.send(e).catch(()=>{});
 }
--- events/shardReady.js
+++ events/shardReady.js
@@ -1,9 +1,12 @@
 const { MessageEmbed } = require('discord.js');
+const { logShard } = require('../console/consoleR');
 
-exports.run = async (client, shard) => {
+exports.run = async (client, shard, guilds) => {
+    logShard(client, 'ready', shard);
     const e = new MessageEmbed()
     .setTitle('Radeon')
-    .setDescription(`Shard ${shard} / ${client.shard.count} - Ready`)
+    .setDescription(`Shard ${shard} / ${client.shard.count} - Ready${guilds ? '\n'+ guilds.join('\n') : ''}`)
     .setColor('GREEN');
-    client.channels.cache.get(client.config.logs.event).send(e).catch(()=>{});
+    const c = client.channels.cache.get(client.config.logs.event);
+    if (c) c.send(e).catch(()=>{});
 }
