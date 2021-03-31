5c55ac352f05e360d421bd0b5f0649a79f8c4d65
NaruDevnote <pokeraven4@gmail.com>
2021-03-31 15:56:26 +0100

    fix(event): clean up

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -2,20 +2,22 @@ const { MessageEmbed } = require('discord.js');
 const Guild = require('../schemas/guild-schema');
 
 exports.run = async (client, guild, user) => {
-    const { modLogs } = await Guild.findOne({guildID: guild.id});
+    const db = await Guild.findOne({ guildID: guild.id });
+    const { modLogs } = db;
     if (!modLogs.channel || !modLogs.bans) return;
-    const { reason } = await guild.fetchBan(user);
+    const info = await guild.fetchBan(user);
     let mod = 'Unknown';
-    if (/^.+#\d{4}:/gi.test(reason)) mod = reason.split(' ').shift().replace(':','');
+    if (/^.+#\d{4}:/gi.test(info.reason)) mod = reason.split(' ').shift().replace(':','');
     const embed = new MessageEmbed()
     .setTitle('Member Banned')
     .setThumbnail(user.displayAvatarURL({dynamic: true}))
     .addFields(
         {name: 'User', value: `• ${user.tag}\n• ${user.id}`, inline: true},
         {name: 'Moderator', value: mod, inline: true},
-        {name: 'Reason', value: reason ? reason.replace(/^.+#\d{4}:/gi,'') : '(No Reason Specified)', inline: false}
+        {name: 'Reason', value: info.reason ? info.reason.replace(/^.+#\d{4}:/gi,'') : '(No Reason Specified)', inline: false}
     )
     .setColor('RED')
     .setTimestamp();
-    guild.channels.cache.get(modLogs.channel).send(embed).catch(()=>{});
+    const c = guild.channels.cache.get(modLogs.channel);
+    if (c) c.send(embed).catch(()=>{});
 }
