c9cf383740eba0e1820a7ed7738aace2273509fe
NaruDevnote <pokeraven4@gmail.com>
2021-04-04 17:05:52 +0100

    refactor(event): kick log with auditlogs

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,21 +2,35 @@ const { MessageEmbed } = require('discord.js');
 const Guild = require('../schemas/guild-schema');
 
 exports.run = async (client, member) => {
-    const { modLogs } = await Guild.findOne({guildID: member.guild.id});
-    if (!modLogs.channel || !modLogs.kicks) return;
-    let entry = await member.guild.fetchAuditLogs({user:member.user, type:'MEMBER_KICK'});
-    entry = entry.entries.first();
-    if (!entry) return;
-    const { executor, reason } = entry;
-    const embed = new MessageEmbed()
-    .setTitle('Member Kicked')
-    .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
-    .addFields(
-        {name: 'User', value: `• ${member.user.tag}\n• ${member.user.id}`, inline: true},
-        {name: 'Moderator', value: executor, inline: true},
-        {name: 'Reason', value: reason ?? '(No Reason Specified)', inline: false}
-    )
-    .setColor('GREY')
-    .setTimestamp();
-    client.channels.cache.get(modLogs.channel).send(embed).catch(()=>{});
+    const db = await Guild.findOne({ guildID: member.guild.id });
+    const { modLogs } = db;
+    if (!modLogs.channel && !modLogs.kicks) return;
+    let count = 0;
+    let audit;
+    while (!audit) {
+        audit = await member.guild.fetchAuditLogs({ type: 'MEMBER_KICK', limit: 3 });
+        if (!audit) {
+            count++;
+            if (count == 3) break;
+            setTimeout(() => {}, 3000);
+        } else {
+            audit = audit.entries.first();
+            break;
+        }
+    }
+    if (audit) {
+        const { reason, executor } = audit;
+        const embed = new MessageEmbed()
+        .setTitle('Member Kicked')
+        .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
+        .addFields(
+            {name: 'User', value: `• ${member.user.tag}\n• ${member.user.id}`, inline: true},
+            {name: 'Moderator', value: `• ${executor.tag}\n• ${executor.id}`, inline: true},
+            {name: 'Reason', value: reason || '(No Reason Specified)', inline: false}
+        )
+        .setColor('GREY')
+        .setTimestamp();
+        const c = member.guild.channels.cache.get(modLogs.channel);
+        if (c) c.send(embed).catch(()=>{});
+    }
 }
