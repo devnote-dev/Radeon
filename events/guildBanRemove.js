c5ff5b7c1c007c78bc275d957bb0f3e9cad38c42
NaruDevnote <pokeraven4@gmail.com>
2021-04-04 17:05:31 +0100

    refactor(events): ban add/remove log with auditlogs

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -2,24 +2,35 @@ const { MessageEmbed } = require('discord.js');
 const Guild = require('../schemas/guild-schema');
 
 exports.run = async (client, guild, user) => {
-    const { modLogs } = await Guild.findOne({guildID: guild.id});
-    if (!modLogs.channel || !modLogs.bans) return;
-    let entry = await guild.fetchAuditLogs({user:user, type:'MEMBER_BAN_REMOVE'});
-    entry = entry.entries.first();
-    let executor, reason;
-    if (entry) {
-        executor = entry.executor;
-        reason = entry.reason;
+    const db = await Guild.findOne({ guildID: guild.id });
+    const { modLogs } = db;
+    if (!modLogs.channel && !modLogs.bans) return;
+    let count = 0;
+    let audit;
+    while (!audit) {
+        audit = await guild.fetchAuditLogs({ type: 'MEMBER_BAN_REMOVE', limit: 3 });
+        if (!audit) {
+            count++;
+            if (count == 3) break;
+            setTimeout(() => {}, 3000);
+        } else {
+            audit = audit.entries.first();
+            break;
+        }
     }
-    const embed = new MessageEmbed()
-    .setTitle('Member Unbanned')
-    .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
-    .addFields(
-        {name: 'User', value: `• ${user.tag}\n• ${user.id}`, inline: true},
-        {name: 'Moderator', value: executor ? executor.tag : 'Unknown', inline: true},
-        {name: 'Reason', value: reason ?? '(No Reason Specified)', inline: false}
-    )
-    .setColor('GREEN')
-    .setTimestamp();
-    guild.channels.cache.get(modLogs.channel).send(embed).catch(()=>{});
+    if (audit) {
+        const { reason, executor } = audit;
+        const embed = new MessageEmbed()
+        .setTitle('Member Unbanned')
+        .setThumbnail(user.displayAvatarURL({dynamic: true}))
+        .addFields(
+            {name: 'User', value: `• ${user.tag}\n• ${user.id}`, inline: true},
+            {name: 'Moderator', value: `• ${executor.tag}\n• ${executor.id}`, inline: true},
+            {name: 'Reason', value: reason || '(No Reason Specified)', inline: false}
+        )
+        .setColor('GREEN')
+        .setTimestamp();
+        const c = guild.channels.cache.get(modLogs.channel);
+        if (c) c.send(embed).catch(()=>{});
+    }
 }
