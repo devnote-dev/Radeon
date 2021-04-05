dce32201c5e9b3a243372eec72d326d9a37cfa9c
NaruDevnote <pokeraven4@gmail.com>
2021-04-05 02:51:40 +0100

    chore(*): Antiraid foundations

---
+++ schemas/antiraid-schema.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const antiraidSchema = Schema({
+    guildID:{
+        type:        String,
+        required:    true
+    },
+    active:          Boolean,
+    channel:         String,
+    locked:          Boolean,
+    syncWarns:       Boolean,
+    syncBans:        Boolean,
+    config:{
+        thres:{
+            type:    Number,
+            default: 5
+        },
+        spammers:    Boolean,
+        selfbots:    Boolean,
+        raiders:     Boolean,
+        alters:      Boolean
+    }
+});
+
+module.exports = model('antiraid', antiraidSchema, 'AntiRaid');
