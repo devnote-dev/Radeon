9ae50d87a97e37e07180c6b061d3fb7b004f0d63
NaruDevnote <pokeraven4@gmail.com>
2021-04-12 13:35:58 +0100

    fix(cmds): kick/ban/unban reworks

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -1,36 +1,49 @@
-require('discord.js');
+const { GuildMember, User } = require('discord.js');
 
 module.exports = {
     name: 'ban',
     tag: 'Bans a member from the server',
-    description: 'Bans a member from the server. `-dd <number>` flag is for the number of days the user\'s messages should be deleted (max 7).',
-    usage: 'ban <User:Mention/ID> <Reason:text> [-dd <number>]',
+    description: 'Bans a member from the server. `-dd <Number>` flag is for the number of days the user\'s messages should be deleted (max 7).',
+    usage: 'ban <User:Mention/ID> <Reason:Text> [-dd <Number>]',
     cooldown: 2,
     userPerms: 4,
     botPerms: 4,
     guildOnly: true,
     run: async (client, message, args) => {
-        if (!args.length) return client.errEmb('No User Specified.\n```\nban <User:Mention/ID> <Reason:text> [-dd <number>]\n```', message);
-        let target = message.mentions.users.first() || client.users.cache.get(args[0]) || args[0];
-        let tag;
-        if (target.tag) tag = target.tag; else tag = target;
-        if (target.id) target = target.id;
-        if (message.author.id === target) return client.errEmb('You can\'t ban yourself.', message);
-        if (args.length < 2) return client.errEmb('A reason is required for this command.', message);
+        if (!args.length) return client.errEmb('Insufficient Arguments.\n```\nban <User:Mention/ID> <Reason:Text> [-dd <Number>]\n```', message);
+        let target = message.mentions.users.first() || client.users.cache.get(args[0]);
+        let fetched;
+        if (!target) {
+            try {
+                fetched = await client.users.fetch(args[0]);
+            } catch {}
+            if (target) target = fetched.id; else target = args[0];
+        } else {
+            target = target.id;
+        }
+        if (target == message.author.id) return client.errEmb('You can\'t ban yourself.', message);
+        if (target == client.user.id) return client.errEmb('I can\'t ban myself.', message);
+        if (args.length < 2) return client.errEmb('A Reason is Required for this Command.', message);
         let reason = args.slice(1).join(' ');
         let ddays = 0;
-        if (/-dd\s+\d$/gi.test(reason)) {
-            ddays = parseInt(/\d$/g.exec(reason)[0]);
-            reason = reason.replace(/-dd\s+\d$/gi, '');
+        if (/-dd\s+.+$/gi.test(reason)) {
+            ddays = parseInt(/-dd\s+(\d+)$/gi.exec(reason)[1]);
+            reason.replace(/-dd\s+.+$/gi, '');
         }
         if (isNaN(ddays) || ddays < 0 || ddays > 7) ddays = 0;
-        if (target.bannable) return client.errEmb(`User \`${tag}\` could not be banned.`, message);
+        if (!target.bannable) return client.errEmb('User cannot not be Banned.', message);
+        if (message.guild.fetchBan(target)) return client.errEmb('User is already Banned.', message);
         try {
-            if (client.users.cache.get(target)) await client.users.cache.get(target).send({embed:{title:'You have been Banned!',description:`**Reason:** ${reason}`,color:0x1e143b,footer:{text:`Sent from ${message.guild.name}`, icon_url:message.guild.iconURL({dynamic:true})}}}).catch(()=>{});
-            message.guild.members.ban(target, {days:ddays, reason:message.author.tag +': '+ reason});
-            return client.checkEmb(`Successfully Banned \`${tag}\`!`, message);
+            if (target instanceof User) target.send(client.actionDM('Banned', message, reason)).catch(()=>{});
+            let mS = await message.guild.members.ban(target, {days: ddays, reason: `${message.author.tag}: ${reason}`});
+            if (mS instanceof GuildMember) {
+                mS = mS.user.tag;
+            } else if (mS instanceof User) {
+                mS = mS.tag;
+            }
+            return client.checkEmb(`Successfully Banned \`${mS}\``, message);
         } catch {
-            return client.errEmb(`Unknown: Failed Banning Member \`${tag}\``, message);
+            return client.errEmb(`Unknown: Failed Banning User \`${fetched ? fetched.tag : target}\``, message);
         }
     }
 }
--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -4,21 +4,23 @@ module.exports = {
     name: 'kick',
     tag: 'Kicks a member from the server',
     description: 'Kicks a member from the server.',
-    usage: 'kick <User:Mention/ID> [Reason:text]',
+    usage: 'kick <User:Mention/ID> [Reason:Text]',
     cooldown: 2,
     userPerms: 2,
     botPerms: 2,
     guildOnly: true,
     run: async (client, message, args) => {
-        if (!args.length) return client.errEmb('No User Specified.\n```\nkick <User:Mention/ID> [Reason:text]\n```', message);
-        const target = message.mentions.members.first() || message.guild.member(args[0]);
+        if (!args.length) return client.errEmb('No User Specified.\n```\nkick <User:Mention/ID> [Reason:Text]\n```', message);
+        const target = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if (!target) return client.errEmb('User is not a member of this server.', message);
+        if (target.user.id == message.author.id) return client.errEmb('You can\'t kick yourself.', message);
+        if (target.user.id == client.user.id) return client.errEmb('I can\'t kick myself.', message);
         let reason = '(No Reason Specified)';
         if (args.length > 1) reason = args.slice(1).join(' ');
-        if (!target.kickable) return client.errEmb(`User \`${target.user.tag}\` could not be kicked (user may have a role higher than the bot).`, message);
+        if (!target.kickable) return client.errEmb(`User \`${target.user.tag}\` cannot be Kicked (check role and bot perms).`, message);
         try {
-            await target.send({embed:{title:'You have been kicked!',description:`**Reason:** ${reason}`,color:0x1e143b,footer:{text:`Sent from ${message.guild.name}`, icon_url:message.guild.iconURL({dynamic: true})}}}).catch(()=>{});
-            target.kick(message.author.tag +': '+ reason);
+            await target.send(client.actionDM('Kicked', message, reason)).catch(()=>{});
+            await target.kick(`${message.author.tag}: ${reason}`);
             return client.checkEmb(`Successfully Kicked \`${target.user.tag}\`!`, message);
         } catch {
             return client.errEmb(`Unknown: Failed Kicking Member \`${target.user.tag}\``, message);
