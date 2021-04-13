324705af2d71f9b55757ededdae0644e298e9b2f
NaruDevnote <pokeraven4@gmail.com>
2021-04-13 15:47:08 +0100

    fix(cmd): switch flags and advanceClean

--- commands/Moderation/clean.js
+++ commands/Moderation/clean.js
@@ -1,5 +1,5 @@
 require('discord.js');
-const cleanCheck = require('../../functions/cleanCheck');
+const advanceClean = require('../../functions/advanceClean');
 
 module.exports = {
     name: 'clean',
@@ -12,28 +12,28 @@ module.exports = {
     botPerms: 8192,
     guildOnly: true,
     run: async (client, message, args) => {
-        if (!args.length) return client.errEmb('No Amount Specified.\n```\nclean <Amount:Number>\nclean <Amount:Number> [User:Mention/ID]\nclean <Amount:Number> [...Flags]\n```\n**Flags:**\n`-users` - Users only\n`-bots` - Bots Only\n`-r <Regex>` - Messages that match a certain regex.', message);
+        if (!args.length) return client.errEmb('No Amount Specified.\n```\nclean <Amount:Number>\nclean <Amount:Number> [User:Mention/ID]\nclean <Amount:Number> [...Flags]\n```\n**Flags:**\n`-users` - Users only\n`-bots` - Bots Only\n`-has <Word>` - Messages that contain a specific word/char', message);
         let amount = parseInt(args[0]);
         if (isNaN(amount) || amount < 1 || amount > 100) return client.errEmb('Invalid Amount Specified. Amount must be a number between 1 and 100.', message);
         if (args.length > 1) {
-            var flagArgs = args.splice(1);
-            let target = '', flagUsers = false, flagBots = false, flagNopin = false, flagRegex = '';
+            var flagArgs = args.slice(1);
+            let target = '', flagUsers = false, flagBots = false, flagNopin = false, flagHas = '';
             for (const arg of flagArgs) {
                 if (/(<@!?)?\d{17,19}>?/g.test(arg)) target = /(?:<@!?)?(\d{17,19})>?/g.exec(arg)[1];
                 if (arg.toLowerCase() === '-users') flagUsers = true;
                 if (arg.toLowerCase() === '-bots') flagBots = true;
                 if (arg.toLowerCase() === '-nopin') flagNopin = true;
-                if (arg.toLowerCase() === '-r') {
+                if (arg.toLowerCase() === '-has') {
                     const index = flagArgs.indexOf(arg);
                     if (index < flagArgs.length) {
-                        flagRegex = flagArgs[index+1].trim();
+                        flagHas = flagArgs[index+1].trim();
                     }
                 }
             }
             if (flagUsers && flagBots) return client.errEmb('Both Users & Bots Flags Specified, pick one.', message);
             try {
-                const res = await cleanCheck(message, amount, {target:target, flagUsers:flagUsers, flagBots:flagBots, flagNopin:flagNopin, flagRegex:flagRegex});
-                return client.checkEmb(`Deleted \`${res}\` Messages!`, message).then(m => m.delete({timeout: 3000}));
+                const res = await advanceClean(message, amount, {target:target, flagUsers:flagUsers, flagBots:flagBots, flagNopin:flagNopin, flagHas:flagHas});
+                return client.checkEmb(`Deleted \`${res}\` Messages!`, message).then(m => setTimeout(() => m.delete(), 3000));
             } catch (err) {
                 return client.errEmb(err.message, message);
             }
@@ -41,7 +41,7 @@ module.exports = {
             if (amount < 100) amount += 1;
             try {
                 const res = await message.channel.bulkDelete(amount, true);
-                return client.checkEmb(`Deleted \`${res.size}\` Messages!`, message).then(m => m.delete({timeout: 3000}));
+                return client.checkEmb(`Deleted \`${res.size}\` Messages!`, message).then(m => setTimeout(() => m.delete(), 3000));
             } catch (err) {
                 return client.errEmb(err.message, message);
             }
