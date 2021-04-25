a279bd3334e7cbe5c4ba82fdda522091e8d8f0c7
NaruDevnote <pokeraven4@gmail.com>
2021-04-25 14:02:12 +0100

    fix(cmd): status.js duration fix

--- src/commands/Maintenance/status.js
+++ src/commands/Maintenance/status.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const { version: dcversion } = require('discord.js');
 const { join } = require('path');
 const { version } = require(join(process.cwd(), 'package.json'));
 const { toDurationDefault } = require('../../functions/functions');
@@ -7,6 +7,7 @@ module.exports = {
     name: 'status',
     tag: 'Sends Radeon\'s status',
     description: 'Sends the bot\'s status for that guild.',
+    cooldown: 3,
     run: async (client, message) => {
         const { heapUsed, heapTotal } = process.memoryUsage();
         const embed = new Discord.MessageEmbed()
@@ -15,14 +16,14 @@ module.exports = {
         .setColor(0x1e143b)
         .addField('Owners', `<@${client.config.botOwners.join('>\n<@')}>`, true)
         .addField('Version', version, true)
-        .addField('Discord.js', Discord.version, true)
+        .addField('Discord.js', dcversion, true)
         .addField('NodeJS', process.version, true)
         .addField('Shards', client.shard.count, true)
         .addField('Servers', client.guilds.cache.size, true)
         .addField('Channels', client.channels.cache.size, true)
         .addField('Users', client.users.cache.size, true)
         .addField('Stats', `${client.stats.commands} Commands ran\n${client.stats.events} Events processed\n${client.stats.messages} Messages seen\n${client.stats.background} Background Tasks ran`, true)
-        .addField('Uptime', toDurationDefault(client.uptime), false)
+        .addField('Uptime', toDurationDefault(Date.now() - client.uptime).replace('ago', ''), false)
         .addField('Memory', `${Math.floor(heapUsed / 1024)}/${Math.floor(heapTotal / 1024)} MB (${Math.floor((heapUsed / heapTotal) * 100)}%)`)
         .setFooter(`Triggered By ${message.author.tag}`, message.author.displayAvatarURL());
         return message.channel.send(embed);
