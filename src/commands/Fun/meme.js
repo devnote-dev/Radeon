53e3092a1438c1ab830a35cc14bf0ca8556eca69
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2021-04-28 09:36:22 -0700

    Create meme.js

---
+++ src/commands/Fun/meme.js
@@ -0,0 +1,23 @@
+const { redditPost } = require('../../functions/reddit');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'meme',
+    aliases: ['m'],
+    tag: 'It\'s a meme...',
+    description: 'It gives you memes....',
+    usage: 'meme',
+    cooldown: 5,
+    run: async (client, message, args) => {
+       let post = await redditPost(["meme", "dankmeme"])
+       if (!post) return client.errEmb("No meme was found", message)
+
+        const embed = new MessageEmbed()
+        .setTitle(`${post.title}`)
+        .setURL(`https://reddit.com${post.permalink}`)
+        .setColor(0x1e143b)
+        .setImage(post.url_overridden_by_dest)
+        .setFooter(`👍 ${post.ups} 💬 ${post.num_comments} - ${post.subreddit_name_prefixed}`);
+        return message.reply(embed);
+    }
+}
