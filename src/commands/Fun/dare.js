7aea07b33d8652a9cf1618eadd4a274866debd20
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2021-04-27 21:54:25 -0700

    Update dare.js

--- src/commands/Fun/dare.js
+++ src/commands/Fun/dare.js
@@ -7,18 +7,18 @@ module.exports = {
     tag: 'Returns a random dare.',
     description: 'Returns a random dare',
     usage: 'dare [pg|pg13|r] [d|irls]',
-    cooldown: 3,
+    cooldown: 5,
     run: async (client, message, args) => {
         // setting rating
         let rating = await choose(args, ["pg", "pg13", "r"], null);
         let type = choose(args, ["d", "irl"], rating);
 
         let dare = (await request.get(`https://api.truthordarebot.xyz/dare?rating=${rating}&type=${type}`).then(response => response.body)).question;
-        if (!dare)return client.err(message, "Dare", "No dare was returned, try again or get support");
+        if (!dare)return client.errEmb("No dare was returned, try again or get support", message);
 
         const embed = new MessageEmbed()
         .setTitle("Dare")
-        .setColor(0x4B0082)
+        .setColor(0x1e143b)
         .setFooter(`Rating: ${rating} - Type: ${type}`)
         .setDescription(dare);
         return message.reply(embed);
