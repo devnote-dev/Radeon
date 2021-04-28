0224daa542c419ec5a1e497c9005b74a8ee3134e
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2021-04-28 13:42:19 -0700

    Rich Presence default songavailable.js
    if there's no search it'll check for a spotify or yt music rich presence and use that instead

--- src/commands/Fun/songavailable.js
+++ src/commands/Fun/songavailable.js
@@ -1,4 +1,5 @@
 const request = require('node-superfetch')
+const { parseFlags } = require('../../functions/stringParser');
 const { MessageEmbed } = require('discord.js');
 const { YoutubeKey } = require("../../../config.json");
 
@@ -7,37 +8,58 @@ module.exports = {
     aliases: ['songav', 'sav'],
     tag: 'Find what platforms have a song.',
     description: 'Find what platforms have a song.',
-    usage: 'dare [pg|pg13|r] [d|irls]',
+    usage: 'songavailable [song name|-link song share link]',
     cooldown: 20,
     run: async (client, message, args) => {
+        // flags
+        let flags = parseFlags(args.join(" "), [
+            {name: 'link', type: 'string', quotes: false},
+        ])
+
+        // set search to presence
+        let spotifyActivity = message.member.presence.activities.find(activity => activity.name == 'Spotify' || activity.name.toLowerCase() == 'youtube music')
+        if (spotifyActivity && !args.length) {
+            args = [spotifyActivity.details, spotifyActivity.state]
+        }
+
+        // Quick - error
         if (!YoutubeKey) return client.errEmb("No youtube api key found")
         if (!args.length) return client.errEmb("Please provide a search", message)
 
-        const embed = new MessageEmbed()
-        .setColor(0x1e143b);
 
-        let musicVideo = await request.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${args.join(" ")}&type=video&key=${YoutubeKey}`).then(res => res.body)
-        if (!musicVideo.pageInfo.totalResults) return client.errEmb("No results", message);
-        musicVideo = musicVideo.items[0]
+        // get music video
+        let musicVideo;
+        let link;
+        if (!flags[0].value) {
+            let musicVideo = await request.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${args.join(" ")}&type=video&key=${YoutubeKey}`).then(res => res.body)
+            if (!musicVideo.pageInfo.totalResults) return client.errEmb("No results", message);
+            musicVideo = musicVideo.items[0]
+            link = `https://www.youtube.com/watch?v=${musicVideo.id.videoId}`
+        } else {
+            link = flags[0].value
+        }
 
-        embed.setTitle(`Song Available - ${musicVideo.snippet.title}`);
-
-        musicVideo = await request.get(`https://api.song.link/v1-alpha.1/links?url=${encodeURIComponent(`https://www.youtube.com/watch?v=${musicVideo.id.videoId}`)}`).then(res => res.body);
+        // use link to find on song.link to get all platforms
+        musicVideo = await request.get(`https://api.song.link/v1-alpha.1/links?url=${encodeURIComponent(link)}`).then(res => res.body);
         if (!musicVideo) return client.errEmb("No results", message);
 
-        let desc = [];
+        // make good embed ree
+        const embed = new MessageEmbed()
+        .setColor(0x1e143b)
+        let desc = []
         let allPlatformsData = musicVideo.linksByPlatform
-        let allPlatformsNames = Object.keys(allPlatformsData);
+        let allPlatformsNames = Object.keys(allPlatformsData)
         for (let platform of allPlatformsNames) {
-            desc.push(`[${platform}](${allPlatformsData[platform].url})`);
+            desc.push(`[${platform}](${allPlatformsData[platform].url})`)
         }
-        let splitAt = Math.ceil(allPlatformsNames.length / 2);
+        let splitAt = Math.ceil(allPlatformsNames.length / 2)
         embed.addFields(
             {name: "_ _", value: desc.slice(0, splitAt).join("\n"), inline: true},
             {name: "_ _", value: desc.slice(splitAt).join("\n"), inline: true},
         );
-        let thumbKeys = Object.keys(musicVideo.entitiesByUniqueId);
-        embed.setThumbnail(musicVideo.entitiesByUniqueId[thumbKeys[0]].thumbnailUrl);
+        let songData = musicVideo.entitiesByUniqueId[Object.keys(musicVideo.entitiesByUniqueId)[0]]
+        embed.setThumbnail(spotifyActivity ? 'https://i.scdn.co/image/' + spotifyActivity.assets.largeImage.split(':')[1] : songData.thumbnailUrl)
+        embed.setTitle(songData.title + " by " + songData.artistName)
 
         return message.reply(embed);
     }
