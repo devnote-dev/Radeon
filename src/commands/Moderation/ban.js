b60590332cba03fc387023b7aaa2a4d5dc50e082
NaruDevnote <pokeraven4@gmail.com>
2021-05-15 01:54:06 +0100

    fix(cmd): ban.js bannable user fix

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -1,11 +1,12 @@
 const { GuildMember, User } = require('discord.js');
+const { parseFlags } = require('../../functions/stringParser');
 
 module.exports = {
     name: 'ban',
     tag: 'Bans a member from the server',
     description: 'Bans a member from the server. `-dd <Number>` flag is for the number of days the user\'s messages should be deleted (max 7).',
     usage: 'ban <User:Mention/ID> <Reason:Text> [-dd <Number>]',
-    cooldown: 2,
+    cooldown: 4,
     userPerms: 4,
     botPerms: 4,
     guildOnly: true,
@@ -26,22 +27,21 @@ module.exports = {
         if (args.length < 2) return client.errEmb('A Reason is Required for this Command.', message);
         let reason = args.slice(1).join(' ');
         let ddays = 0;
-        if (/-dd\s+.+$/gi.test(reason)) {
-            ddays = parseInt(/-dd\s+(\d+)$/gi.exec(reason)[1]);
-            reason.replace(/-dd\s+.+$/gi, '');
-        }
-        if (isNaN(ddays) || ddays < 0 || ddays > 7) ddays = 0;
-        if (!target.bannable) return client.errEmb('User cannot not be Banned.', message);
-        if (message.guild.fetchBan(target)) return client.errEmb('User is already Banned.', message);
+        const flag = parseFlags(args.slice(1).join(' '), [{name: 'dd', type: 'int'}]);
+        if (flag[0].value != null) ddays = flag[0].value;
+        if (ddays < 0 || ddays > 7) ddays = 0;
+        if (target.bannable) return client.errEmb('User cannot not be Banned.', message);
+        const banned = await message.guild.fetchBan(target).catch(()=>{});
+        if (banned) return client.errEmb('User is already Banned.', message);
         try {
             if (target instanceof User) target.send(client.actionDM('Banned', message, reason)).catch(()=>{});
-            let mS = await message.guild.members.ban(target, {days: ddays, reason: `${message.author.tag}: ${reason}`});
-            if (mS instanceof GuildMember) {
-                mS = mS.user.tag;
-            } else if (mS instanceof User) {
-                mS = mS.tag;
+            let MS = await message.guild.members.ban(target, {days: ddays, reason: `${message.author.tag}: ${reason}`});
+            if (MS instanceof GuildMember) {
+                MS = MS.user.tag;
+            } else if (MS instanceof User) {
+                MS = MS.tag;
             }
-            return client.checkEmb(`Successfully Banned \`${mS}\``, message);
+            return client.checkEmb(`Successfully Banned \`${MS}\``, message);
         } catch {
             return client.errEmb(`Unknown: Failed Banning User \`${fetched ? fetched.tag : target}\``, message);
         }
