025945b059fe7f5fac0d94baf335f71466b58aae
NaruDevnote <pokeraven4@gmail.com>
2021-05-15 01:56:03 +0100

    chore(event): handle partial fetch

--- src/events/guildMemberUpdate.js
+++ src/events/guildMemberUpdate.js
@@ -2,13 +2,13 @@ require('discord.js');
 const Guild = require('../schemas/guild-schema');
 const Muted = require('../schemas/muted-schema');
 
-exports.run = async (_client, oldMem, newMem) => {
+exports.run = async (_, oldMem, newMem) => {
     const { guild } = oldMem;
-    const gData = await Guild.findOne({guildID: guild.id}).catch(()=>{});
-    if (!gData) return;
-    if (!gData.muteRole) return;
-    const mData = await Muted.findOne({guildID: guild.id}).catch(()=>{});
+    const gData = await Guild.findOne({ guildID: guild.id }).catch(()=>{});
+    if (!gData || !gData.muteRole) return;
+    const mData = await Muted.findOne({ guildID: guild.id }).catch(()=>{});
     if (!mData.mutedList.length) return;
+    if (newMem.partial) newMem = await newMem.fetch();
     if (
         oldMem.roles.cache.has(gData.muteRole)
         && !newMem.roles.cache.has(gData.muteRole)
