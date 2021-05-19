e841b5581f9cbafb62f63239b707d622a5af8328
NaruDevnote <pokeraven4@gmail.com>
2021-05-19 01:38:48 +0100

    feat(cmd): test ping slash

--- src/commands/Maintenance/ping.js
+++ src/commands/Maintenance/ping.js
@@ -1,11 +1,17 @@
-require('discord.js');
-
 module.exports = {
     name: 'ping',
     tag: 'Sends Radeon\'s ping!',
-    description: 'Sends Radeon\'s ping.',
+    description: 'Sends Radeon\'s ping!',
+    appdata:{
+        name: 'ping',
+        description: 'Sends Radeon\'s ping!'
+    },
     run: async (client, message) => {
         const msg = await message.channel.send({embed:{color:0x1e143b,thumbnail:{url:'https://cdn.discordapp.com/emojis/786661451385274368.gif?v=1'}}});
         return msg.edit({embed:{title:'🏓 Pong!',description:`Websocket: ${client.ws.ping}ms\nAPI: ${msg.createdTimestamp - message.createdTimestamp}ms`,color:0x1e143b}});
     }
+}
+
+module.exports.appres = (client, int) => {
+    return int.reply(`🏓 Pong!\nWS: ${client.ws.ping}ms`, { ephemeral: true });
 }
