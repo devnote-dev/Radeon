73d35a50f6dffe63b5204907cbd5c6d741a6b231
NaruDevnote <pokeraven4@gmail.com>
2021-06-09 03:57:09 +0100

    style(event): shorten interaction code

--- src/events/interaction.js
+++ src/events/interaction.js
@@ -10,8 +10,7 @@ exports.run = async (client, interaction) => {
     if (!interaction.isCommand()) return;
     try {
         if (client.slash.has(interaction.commandName)) {
-            const slash = client.slash.get(interaction.commandName);
-            await slash(client, interaction);
+            await client.slash.get(interaction.commandName)(client, interaction);
         }
     } catch (err) {
         logError(err, __filename, interaction.user?.id);
