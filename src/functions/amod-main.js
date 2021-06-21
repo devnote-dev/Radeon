ea1f65f4c450f9e285694cc14786c500e65be6cf
NaruDevnote <pokeraven4@gmail.com>
2021-06-21 01:08:01 +0100

    fix(func): cleanup code & get invites working

--- src/functions/amod-main.js
+++ src/functions/amod-main.js
@@ -5,15 +5,15 @@
  */
 
 
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require('discord.js');
 
-function amodEmbed(message, user, channel) {
+function amodEmbed(message, ctx) {
     return new MessageEmbed()
     .setTitle('Automod Triggered')
     .addFields(
         {name: 'Rule', value: message, inline: false},
-        {name: 'User', value: `• ${user.tag}\n• ${user.id}`, inline: true},
-        {name: 'Channel', value: `• ${channel}\n• ${channel.id}`, inline: true}
+        {name: 'User', value: `• ${ctx.author.tag}\n• ${ctx.author.id}`, inline: true},
+        {name: 'Channel', value: `• ${ctx.channel}\n• ${ctx.channel.id}`, inline: true}
     )
     .setColor('ORANGE')
     .setTimestamp();
@@ -28,30 +28,26 @@ module.exports = async (message, automod) => {
         const matches = re.exec(message.content);
         if (matches && matches.length) {
             const invites = await message.guild.fetchInvites();
-            let notFromServer = false;
-            if (invites.size) {
-                invites.forEach(i => notFromServer = matches[1] === i.code);
-            }
-            if (notFromServer) {
+            if (!invites.some(i => i.code === matches[1])) {
                 try {
                     await message.delete().catch(()=>{});
                     if (channel) {
-                        channel.send(amodEmbed(`Invite Code Sent: \`${matches[1]}\``, message.author, message.channel));
+                        channel.send(amodEmbed(`Invite Code Sent: \`${matches[1]}\``, message));
                     }
-                    return message.reply('Invites are not allowed here.');
+                    return message.channel.send(`${message.author} Invites are not allowed here.`);
                 } catch {}
             }
         }
+    }
 
-        if (automod.massMention.active) {
-            if (message.mentions.users.size) {
-                if (message.mentions.users.size >= automod.massMention.thres) {
-                    await message.delete().catch(()=>{});
-                    if (channel) {
-                        channel.send(amodEmbed(`${message.mentions.users.size} Mentioned Users`, message.author, message.channel));
-                    }
-                    return message.reply('Avoid mass-mentioning users.');
+    if (automod.massMention.active) {
+        if (message.mentions.users.size) {
+            if (message.mentions.users.size >= automod.massMention.thres) {
+                await message.delete().catch(()=>{});
+                if (channel) {
+                    channel.send(amodEmbed(`${message.mentions.users.size} Mentioned Users`, message));
                 }
+                return message.reply(`${message.author} Avoid mass-mentioning users.`);
             }
         }
     }
