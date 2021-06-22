5ac2a59c36d0a8bc7baf117ce38158737c67d303
NaruDevnote <pokeraven4@gmail.com>
2021-06-22 23:37:32 +0100

    fix(event): catch logging & switch to preset

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -4,35 +4,23 @@
  * @copyright Radeon Development 2021
  */
 
+
 const { MessageEmbed } = require('discord.js');
 const Guild = require('../schemas/guild-schema');
 const Muted = require('../schemas/muted-schema');
 const Warns = require('../schemas/warn-schema');
+const Presets = require('../schemas/presets');
 
 exports.run = async (client, guild) => {
-    const e = new MessageEmbed()
-    .setDescription(`<:checkgreen:796925441771438080> Joined **${guild.name}** - Active in ${client.guilds.cache.size} Servers!`)
-    .setColor(0x00d134)
-    .setTimestamp();
-    client.channels.cache.get(client.config.logs.joins)?.send(e).catch(()=>{});
-    await new Guild({
-        guildID: guild.id,
-        prefix: client.config.prefix,
-        modLogs:{channel:''},
-        muteRole: '',
-        everyoneRole: '',
-        ignoredChannels: [],
-        ignoredCommands: [],
-        automod:{
-            active:false,
-            channel:'',
-            invites:false,
-            rateLimit:false,
-            massMention:{active:false},
-            badWords:{active:false}
-        }
-    }).save();
+    if (!client.guilds.cache.has(guild.id)) {
+        const e = new MessageEmbed()
+        .setDescription(`<:checkgreen:796925441771438080> Joined **${guild.name}** - Active in ${client.guilds.cache.size} Servers!`)
+        .setColor(0x00d134)
+        .setTimestamp();
+        client.channels.cache.get(client.config.logs.joins)?.send(e).catch(()=>{});
+    }
     console.log(`MONGO | Guild Added: ${guild.name}`);
+    await new Guild(Presets.guildPreset(guild.id)).save();
     await new Muted({ guildID: guild.id }).save();
     await new Warns({ guildID: guild.id }).save();
 }
