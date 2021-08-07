1bdf6fae92c3c7dd01358e7c3fd7047f8519aee4
NaruDevnote <pokeraven4@gmail.com>
2021-08-07 19:33:57 +0100

    fix(cmd): v13 :/

--- src/commands/Maintenance/ping.js
+++ src/commands/Maintenance/ping.js
@@ -15,7 +15,7 @@ module.exports = {
         description: 'Sends Radeon\'s ping!'
     },
     async run(client, message) {
-        const msg = await message.channel.send({embed:{color:0x1e143b,thumbnail:{url:'https://cdn.discordapp.com/emojis/786661451385274368.gif?v=1'}}});
+        const msg = await message.channel.send({ embeds:[ {color:0x1e143b,thumbnail:{url:'https://cdn.discordapp.com/emojis/786661451385274368.gif?v=1'}} ]});
         return msg.edit({embed:{title:'🏓 Pong!',description:`Websocket: ${client.ws.ping}ms\nAPI: ${msg.createdTimestamp - message.createdTimestamp}ms`,color:0x1e143b}});
     }
 }
