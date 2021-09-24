d7fb74068471cc393b2fdd783f4710c6c22d1426
NaruDevnote <pokeraven4@gmail.com>
2021-09-24 12:58:13 +0100

    style(handler): formatting & import

--- src/handlers/event.h.js
+++ src/handlers/event.h.js
@@ -6,29 +6,29 @@
 
 const filehound = require('filehound');
 const { parse, join } = require('path');
-const { logError } = require('../dist/console');
+const log = require('../log');
 
 module.exports = async client => {
     let loaded = 0;
 
     filehound.create()
-    .path(join(process.cwd(),'src','events'))
-    .ext('.js')
-    .findSync()
-    .forEach(async e => {
-        const event = require(e);
-        loaded++;
-        try {
-            client.on(parse(e).base.split('.')[0], async (...args) => {
-                client.stats.events++;
-                await event.run(client, ...args);
-            });
-        } catch (err) {
-            logError(err, __filename);
-        }
-    });
+        .path(join(process.cwd(),'src','events'))
+        .ext('.js')
+        .findSync()
+        .forEach(async e => {
+            const event = require(e);
+            loaded++;
+            try {
+                client.on(parse(e).base.split('.')[0], async (...args) => {
+                    client.stats.events++;
+                    await event.run(client, ...args);
+                });
+            } catch (err) {
+                log.error(err, __filename);
+            }
+        });
     if (!loaded) {
-        console.log(`\x1b[35mRadeon\x1b[0m | \x1b[31m0\x1b[0m Events Loaded.\nInsufficient Events To Run Radeon. Terminating...`);
+        console.error(`\x1b[35mRadeon\x1b[0m | \x1b[31m0\x1b[0m Events Loaded.\nInsufficient events to run Radeon. Terminating...`);
         process.exit();
     } else {
         client.stats._events = loaded;
