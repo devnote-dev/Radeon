eda0af475cb1ef1d7963bcbaaa973ca95de82858
NaruDevnote <pokeraven4@gmail.com>
2021-09-24 12:58:50 +0100

    fix(cmd): update eval imports

--- src/commands/Admin/eval.js
+++ src/commands/Admin/eval.js
@@ -6,20 +6,20 @@
 const Discord = require('discord.js');
 const { Util } = Discord;
 const { inspect } = require('util');
-const { logAdmin } = require('../../dist/console');
-const funcs = require('../../dist/functions');
+const log = require('../../log');
+const funcs = require('../../functions');
 
 module.exports = {
     name: 'eval',
     guildOnly: true,
     modOnly: 1,
+
     async run(client, message, args) {
         const { author, member, channel, guild } = message;
         if (!args.length) return client.errEmb('No Code Provided.', message);
-        const _path = `${guild.id}/${channel.id}`;
         let code = args.raw.join(' ');
         if (/^`{3}(?:.+)?(?:\n+)?[\s\S]+(?:\n+)?`{3}$/gmi.test(code)) code = code.replace(/^```(?:[\S\n]+)?|```$/gmi, '');
-        logAdmin('eval', _path, author.id, code);
+        log.admin('eval', message, author.id, code);
         if (/while\s*\(\s*true\s*\)\s*\{\s*\}/gi.test(code)) return client.errEmb('Cannot execute infinite loop.', message);
         try {
             let evaled;
