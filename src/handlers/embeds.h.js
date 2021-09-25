131163f4c35d1626960bdf152e5120a39dd8d4b4
NaruDevnote <pokeraven4@gmail.com>
2021-09-25 20:58:27 +0100

    feat(embeds): formatting & new ttl

--- src/handlers/embeds.h.js
+++ src/handlers/embeds.h.js
@@ -3,52 +3,52 @@
  * @copyright Radeon Development 2021
  */
 
-const { Guild, Message, MessageEmbed } = require('discord.js');
+const { Message, MessageEmbed } = require('discord.js');
 
 module.exports = async client => {
-    client.checkEmb = (msg, ctx) => {
+    client.check = (msg, ctx, ttl) => {
         const e = new MessageEmbed()
-        .setDescription('<:checkgreen:796925441771438080> '+ msg)
-        .setColor(0x00d134);
+            .setDescription(msg)
+            .setColor(0x00d134);
         if (ctx instanceof Message) {
-            return ctx.channel.send({ embeds: [e] });
+            return ctx.channel.send({ embeds:[e] }).then(m => {
+                if (ttl) setTimeout(() => m.delete(), ttl * 1000);
+            });
         } else {
-            return ctx.send({ embeds: [e] });
+            return ctx.send({ embeds:[e] }).then(m => {
+                if (ttl) setTimeout(() => m.delete(), ttl * 1000);
+            });
         }
     }
 
-    client.errEmb = (msg, ctx) => {
+    client.error = (msg, ctx, ttl) => {
         const e = new MessageEmbed()
-        .setDescription('<:crossred:796925441490681889> '+ msg)
-        .setColor(0xd10000);
+            .setDescription(msg)
+            .setColor(0xd10000);
+        let m;
         if (ctx instanceof Message) {
-            return ctx.channel.send({ embeds: [e] });
+            ctx.channel.send({ embeds:[e] }).then(m => {
+                if (ttl) setTimeout(() => m.delete(), ttl * 1000);
+            });
         } else {
-            return ctx.send({ embeds: [e] });
+            return ctx.send({ embeds:[e] }).then(m => {
+                if (ttl) setTimeout(() => m.delete(), ttl * 1000);
+            });
         }
     }
 
-    client.infoEmb = (msg, ctx) => {
+    client.info = (msg, ctx, ttl) => {
         const e = new MessageEmbed()
-        .setDescription('<:info:846179402773168159> '+ msg)
-        .setColor(0x0054d1);
+            .setDescription('<:info:846179402773168159> '+ msg)
+            .setColor(0x0054d1);
         if (ctx instanceof Message) {
-            return ctx.channel.send({ embeds: [e] });
+            return ctx.channel.send({ embeds:[e] }).then(m => {
+                if (ttl) setTimeout(() => m.delete(), ttl * 1000);
+            });
         } else {
-            return ctx.send({ embeds: [e] });
+            return ctx.send({ embeds:[e] }).then(m => {
+                if (ttl) setTimeout(() => m.delete(), ttl * 1000);
+            });
         }
-    }
-
-    client.actionDM = (action, ctx, message) => {
-        const e = new MessageEmbed()
-        .setTitle(`You have been ${action}!`)
-        .setDescription(message)
-        .setColor(0x1e143b);
-        if (ctx instanceof Guild) {
-            e.setFooter(`Sent from ${ctx.name}`, ctx.iconURL({dynamic: true}));
-        } else if (ctx instanceof Message) {
-            e.setFooter(`Sent from ${ctx.guild.name}`, ctx.guild.iconURL({dynamic: true}));
-        }
-        return e;
     }
 }
