d863f7545cd6f55ecda673d7512eef1e2aab2e4f
NaruDevnote <pokeraven4@gmail.com>
2021-09-25 20:57:10 +0100

    style(handler): indents

--- src/handlers/command.h.js
+++ src/handlers/command.h.js
@@ -11,24 +11,24 @@ module.exports = async client => {
     const failed = [];
 
     filehound.create()
-    .path(join(process.cwd(),'src','commands'))
-    .ext('.js')
-    .findSync()
-    .forEach(cmd => {
-        try {
-            const command = require(cmd);
-            if (command.name) {
-                client.commands.set(command.name, command);
-                if (command.appdata) client.slash.set(command.appdata.name, command.appres);
-            } else if (command.appdata) {
-                client.slash.set(command.appdata.name, command.appres);
-            } else {
+        .path(join(process.cwd(),'src','commands'))
+        .ext('.js')
+        .findSync()
+        .forEach(cmd => {
+            try {
+                const command = require(cmd);
+                if (command.name) {
+                    client.commands.set(command.name, command);
+                    if (command.slash) client.slash.set(command.name, command.slash);
+                } else if (command.slash) {
+                    client.slash.set(command.name, command.slash);
+                } else {
+                    failed.push(parse(cmd).base);
+                }
+                if (command.aliases && Array.isArray(command.aliases)) command.aliases.forEach(a => client.aliases.set(a, command.name));
+            } catch {
                 failed.push(parse(cmd).base);
             }
-            if (command.aliases && Array.isArray(command.aliases)) command.aliases.forEach(a => client.aliases.set(a, command.name));
-        } catch {
-            failed.push(parse(cmd).base);
-        }
-    });
+        });
     client.stats._failed = failed;
 }
