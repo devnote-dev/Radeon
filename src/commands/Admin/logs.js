b8aef8a5b9c8e1c46958094a26b8478419df9554
NaruDevnote <pokeraven4@gmail.com>
2021-09-25 20:59:27 +0100

    refactor(cmd): cmdlogs -> logs & fixes

--- src/commands/Admin/logs.js
+++ src/commands/Admin/logs.js
@@ -8,20 +8,21 @@ const { readFileSync, writeFileSync } = require('fs');
 const { join } = require('path');
 
 module.exports = {
-    name: 'cmdlogs',
+    name: 'logs',
     guildOnly: true,
     modOnly: 4,
+
     async run(client, message, args) {
         if (args.length) {
             if (args.lower[0] === 'last') {
                 message.delete().catch(()=>{});
                 const buff = readFileSync(join(__dirname, '_logs.txt'));
-                const att = new MessageAttachment(buff, 'radeon_pastlogs.txt');
+                const att = new MessageAttachment(buff, 'radeon_cmd_logs.txt');
                 try {
                     await message.author.send({ content: 'Radeon Command Logs', files: [att] });
-                    return client.infoEmb('I have DMed you the file.', message).then(m => setTimeout(() => m.delete(), 5000));
+                    return client.info('I have DMed you the file.', message, 5);
                 } catch {
-                    return client.errEmb('Failed Sending DM. Check that your DMs are enabled.', message);
+                    return client.error('Failed Sending DM. Check that your DMs are enabled.', message);
                 }
             } else if (args.lower[0] === 'save') {
                 let content = '';
@@ -37,12 +38,12 @@ module.exports = {
             client.cmdlogs.forEach(
                 log => content += `USER: ${log.user}\nCMD: ${log.command}\nCHANNEL: ${log.channel.id} - ${log.channel.type}\nTIME: ${log.time}\n\n`
             );
-            const att = new MessageAttachment(Buffer.from(content), 'radeon_cmdlogs.txt');
+            const att = new MessageAttachment(Buffer.from(content), 'radeon_cmd_logs.txt');
             try {
-                await message.author.send({ content: 'Radeon Command Logs', files: [att] });
-                return client.infoEmb('I have DMed you the file.', message).then(m => setTimeout(() => m.delete(), 5000));
+                await message.author.send({ content: 'Radeon Command Logs', files:[att] });
+                return client.info('I have DMed you the file.', message, 5);
             } catch {
-                return client.errEmb('Failed Sending DM. Check that your DMs are enabled.', message);
+                return client.error('Failed Sending DM. Check that your DMs are enabled.', message);
             }
         }
     }
