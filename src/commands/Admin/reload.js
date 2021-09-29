bea437b261aafba8821121262a5b9e16a5e521ef
NaruDevnote <pokeraven4@gmail.com>
2021-09-29 23:06:54 +0100

    fix(cmd): reload quick fix

--- src/commands/Admin/reload.js
+++ src/commands/Admin/reload.js
@@ -34,7 +34,7 @@ module.exports = {
                 category = 'Moderation'; break;
             case 'ss':
             case 'settings':
-                category = 'Server Settings'; break;
+                category = 'Settings'; break;
             case 'f':
             case 'fun':
                 category = 'Fun'; break;
@@ -43,7 +43,7 @@ module.exports = {
         }
         if (!category) return client.error('Unknown command category specified.', message);
         if (!cmd) return client.error('Unknown command specified.', message);
-        log.admin('reload', message, message.autor.id, cmd.name);
+        // log.admin('reload', message, message.autor.id, cmd.name);
         try {
             delete require.cache[require.resolve(`../${category}/${cmd.name}.js`)];
             client.commands.delete(cmd);
