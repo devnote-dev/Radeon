142bffa21260692563f11b6186eedf7c075a461f
devnote-dev <pokeraven4@gmail.com>
2021-10-16 12:02:28 +0100

    refactor(cmd): attempt to fix mute

--- src/commands/Moderation/mute.js
+++ src/commands/Moderation/mute.js
@@ -20,20 +20,20 @@ module.exports = {
     roleBypass: true,
     async run(client, message, args) {
         const data = await client.db('guild').get(message.guild.id);
-        if (!data) return client.errEmb('Unknown: Failed Connecting To Server Database. Try contacting support.', message);
+        if (!data) return client.error('Unknown: Failed Connecting To Server Database. Try contacting support.', message);
         const { muteRole, modLogs } = data;
-        if (!muteRole) return client.errEmb('Mute role not found/set. You can set one using the `muterole` command.', message);
-        if (args.length < 2) return client.errEmb('Insufficient Arguments.\n```\nmute <User:Mention/ID> [Time:Duration] <Reason:Text>\n```', message);
+        if (!muteRole) return client.error('Mute role not found/set. You can set one using the `muterole` command.', message);
+        if (args.length < 2) return client.error('Insufficient Arguments.\n```\nmute <User:Mention/ID> [Time:Duration] <Reason:Text>\n```', message);
         const target = message.mentions.members.first() || await resolveMember(message, args.raw);
-        if (!target) return client.errEmb('User is not a member of the server.', message);
+        if (!target) return client.error('User is not a member of the server.', message);
         let duration = ms(args.raw[1]);
         if (isNaN(duration) || duration > 31557600000) duration = 'inf';
         let reason = args.raw.slice(2).join(' ');
-        if (target.user.id === message.author.id) return client.errEmb('You cant mute yourself.', message);
-        if (target.user.id === client.user.id) return client.errEmb('I can\'t mute myself.', message);
+        if (target.user.id === message.author.id) return client.error('You cant mute yourself.', message);
+        if (target.user.id === client.user.id) return client.error('I can\'t mute myself.', message);
         if (!reason) reason = '(No Reason Specified)';
-        if (target.permissions.has(8n)) return client.errEmb('Unable to Mute: That user is an administrator.', message);
-        if (message.guild.me.roles.highest.comparePositionTo(muteRole) <= 0) return client.errEmb('Unable to Mute: Cannot manage roles higher or equal to Radeon.', message);
+        if (target.permissions.has(8n)) return client.error('Unable to Mute: That user is an administrator.', message);
+        if (message.guild.me.roles.highest.comparePositionTo(muteRole) <= 0) return client.error('Unable to Mute: Cannot manage roles higher or equal to Radeon.', message);
         try {
             const mData = await client.db('muted').get(message.guild.id);
             if (duration != 'inf') {
@@ -51,7 +51,7 @@ module.exports = {
                 .setFooter(`Sent from ${message.guild.name}`, message.guild.iconURL({ dynamic: true }))
                 .setTimestamp();
             target.user.send({ embeds:[dmEmb] }).catch(()=>{});
-            client.checkEmb(`\`${target.user.tag}\` was muted!`, message);
+            client.check(`\`${target.user.tag}\` was muted!`, message);
             if (modLogs.channel) {
                 const embed = new MessageEmbed()
                     .setTitle('Member Muted')
@@ -68,7 +68,7 @@ module.exports = {
             }
         } catch (err) {
             log.error(err, message);
-            return client.errEmb(`Unknown: Failed muting member \`${target.user.tag}\``, message);
+            return client.error(`Unknown: Failed muting member \`${target.user.tag}\``, message);
         }
     }
 }
