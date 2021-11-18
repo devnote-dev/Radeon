6d443f626df114aac4b1fe52d62749da3ec2aa57
devnote-dev <pokeraven4@gmail.com>
2021-11-18 18:29:03 +0000

    feat(handler): cmd & event handler

---
+++ handler.js
@@ -0,0 +1,25 @@
+/**
+ * @author Piter <https://github.com/piterxyz>
+ * @author Devonte <https://github.com/devnote-dev>
+ * @author Tryharddeveloper <https://github.com/tryharddeveloper>
+ * @copyright 2021 Radeon Development
+ */
+
+const { readdirSync } = require('fs');
+
+module.exports = (client) => {
+    readdirSync('./commands/')
+    .forEach(dir => {
+        readdirSync(`./commands/${dir}/`)
+        .forEach(file => {
+            const cmd = require(`./commands/${dir}/${file}`);
+            client.commands.set(cmd.name, cmd);
+        });
+    });
+
+    readdirSync('./events/')
+    .forEach(file => {
+        const event = require(`./events/${file}`);
+        client.on(file.split('.')[0], (...ctx) => event(client, ...ctx));
+    });
+}
