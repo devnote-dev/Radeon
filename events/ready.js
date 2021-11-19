97bdfbe4e02a4e05ecfdc01fc5087addb9bd820c
devnote-dev <pokeraven4@gmail.com>
2021-11-19 13:52:50 +0000

    feat(event): ready event

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+/**
+ * @author Devonte <https://github.com/devnote-dev>
+ * @copyright 2021 Radeon Development
+ */
+
+const log = require('../log');
+
+module.exports = client => {
+    log.ready(client);
+}
