e5d665c67500aa88b42fd38b61a19364f4a0b2c7
devnote-dev <pokeraven4@gmail.com>
2021-11-19 16:21:46 +0000

    feat(const): add more colours

--- const.js
+++ const.js
@@ -1,7 +1,13 @@
+/**
+ * @author Devonte <https://github.com/devnote-dev>
+ * @copyright 2021 Radeon Development
+ */
+
 exports.col = {
     def: 0x1e143b,
-    red: 0x0,
-    yellow: 0x0,
-    green: 0x0,
-    blue: 0x0
+    red: 0xcf5759,
+    yellow: 0xecd375,
+    green: 0x57cf7a,
+    blue: 0x57a1cf,
+    vio: 0xa591ff
 }
