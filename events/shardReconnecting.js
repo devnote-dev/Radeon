cf60a0678f1a5314f3d198cc2c39c7ba1c5c256e
devnote-dev <pokeraven4@gmail.com>
2021-11-26 23:50:58 +0000

    feat(events): implement shard events

---
+++ events/shardReconnecting.js
@@ -0,0 +1,17 @@
+/**
+ * @author Devonte <https://github.com/devnote-dev>
+ * @copyright 2021 Radeon Development
+ */
+
+const { logs } = require('../config.json');
+
+module.exports = (client, shard) => {
+    if (chan = client.channels.cache.get(logs.event)) {
+        chan.send({
+            embeds:[{
+                description: `${client.const.evt.recon} Reconnecting to shard ${shard}`,
+                color: client.const.col.yellow
+            }]
+        }).catch(()=>{});
+    }
+}
---
+++ events/shardResume.js
@@ -0,0 +1,17 @@
+/**
+ * @author Devonte <https://github.com/devnote-dev>
+ * @copyright 2021 Radeon Development
+ */
+
+const { logs } = require('../config.json');
+
+module.exports = (client, shard) => {
+    if (chan = client.channels.cache.get(logs.event)) {
+        chan.send({
+            embeds:[{
+                description: `${client.const.evt.res} Shard ${shard} resumed`,
+                color: client.const.col.blue
+            }]
+        }).catch(()=>{});
+    }
+}
