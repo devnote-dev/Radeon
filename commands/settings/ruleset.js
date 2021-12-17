1c7db1f78929a5f618965dc312e54d5970a77a46
devnote-dev <pokeraven4@gmail.com>
2021-12-17 01:42:03 +0000

    feat(cmd): implement ruleset command

--- commands/settings/ruleset.js
+++ commands/settings/ruleset.js
@@ -3,6 +3,7 @@
  * @copyright 2021 Radeon Development
  */ 
 
+const { newAutomod } = require('../../database/presets');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
@@ -36,6 +37,57 @@ module.exports = {
                 .setColor(client.const.col.def);
             return channel.send({ embeds:[embed] });
         }
+
+        let sub = args.lower[0], option = args.lower[1];
+        if (sub === 'set') {
+            if (!['invites', 'links', 'spam', 'floods', 'zalgo',
+            'usernames', 'mentions', 'filter'].includes(option)) {
+                return client.error(
+                    [
+                        'Error: Invalid module specified',
+                        '||invites, links, spam, floods, zalgo, usernames, mentions, filter||'
+                    ],
+                    channel
+                );
+            }
+
+            if (args.length < 2)
+                return client.error('No rules specified. See `help ruleset` for more information.', channel);
+
+            const rules = args.lower.slice(2).map(r => r.replace(/,|;/g, ''));
+            if (rules.some(r => !['warn', 'mute', 'kick', 'ban'].includes(r)))
+                return client.error('Invalid rule specified. See `help ruleset` for more information.', channel);
+
+            if (option === 'usernames') option = 'names';
+            const db = client.db('automod');
+            const { rulesets } = await db.get(guild.id);
+            rulesets[option] = rules.map(r => r[0]).join(';');
+            await db.update(guild.id, { rulesets });
+            return client.check(`Successfully updated the ${option} ruleset!`, channel);
+
+        } else if (sub === 'reset') {
+            if (!['invites', 'links', 'spam', 'floods', 'zalgo',
+            'usernames', 'mentions', 'filter'].includes(option)) {
+                return client.error(
+                    [
+                        'Error: Invalid module specified',
+                        '||invites, links, spam, floods, zalgo, usernames, mentions, filter||'
+                    ],
+                    channel
+                );
+            }
+
+            if (option === 'usernames') option = 'names';
+            const db = client.db('automod');
+            const { rulesets } = await db.get(guild.id);
+            const { rulesets: _default } = newAutomod(null);
+            rulesets[option] = _default[option];
+            await db.update(guild.id, { rulesets });
+            return client.check(`Successfully reset the ${option} ruleset!`, channel);
+
+        } else {
+            return client.error('Invalid subcommand. See `help ruleset` for more information.', channel);
+        }
     }
 }
 
@@ -48,5 +100,5 @@ function parseRule(str) {
     }
     const res = [];
     for (const r of str.split(';')) res.push(rules[r]);
-    return res.join(' > ');
+    return '`'+ res.join(' > ') +'`';
 }
